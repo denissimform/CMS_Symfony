ebf3693e210a4c09030e1a9e31730cd40c4e4bd5
dhruval <dhruvaldevaliya@gmail.com>
2023-07-13 13:47:49 +0530

    fix: fixed some bugs in crud

--- src/Controller/AdminDashboard/AdminDashboardController.php
+++ src/Controller/AdminDashboard/AdminDashboardController.php
@@ -6,6 +6,8 @@ use App\Entity\Department;
 use App\Entity\Skills;
 use App\Form\DepartmentType;
 use App\Form\SkillType;
+use App\Repository\DepartmentRepository;
+use App\Repository\SkillsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +22,6 @@ class AdminDashboardController extends AbstractDashboardController
         return $this->render('AdminDashboard/AdminDashboard.html.twig');
     }
 
-    /******************************************SKILLS***************************************************/
 
     #[Route('/admin/dashboard/skills/create', name: 'app_admin_skills_create')]
     public function CreateSkill(EntityManagerInterface $entityManagerInterface, Request $request): Response
@@ -42,47 +43,47 @@ class AdminDashboardController extends AbstractDashboardController
     }
 
     #[Route('/admin/dashboard/skills', name: 'app_admin_skills')]
-    public function Skills(EntityManagerInterface $entityManagerInterface): Response
+    public function Skills(SkillsRepository $skillsRepository): Response
     {
         return $this->render('AdminDashboard/Skills/skills.html.twig',[
-            'skills' => $entityManagerInterface->getRepository(Skills::class)->findBy([
+            'skills' => $skillsRepository->findBy([
                 'isDeleted' => false
             ]) 
         ]);
     }
 
-    #[Route('/admin/dashboard/skills/update/{id}/{flag}', name: 'app_admin_skills_update')]
-    public function Update_Skill(EntityManagerInterface $entityManagerInterface, Skills $skill, Request $request, int $flag): Response
+    #[Route('/admin/dashboard/skills/update/{id}', name: 'app_admin_skills_update')]
+    public function UpdateSkill(EntityManagerInterface $entityManagerInterface, Skills $skill, Request $request): Response
     {
-        if($flag==1){
-            $form = $this->createForm(SkillType::class,$skill);
-            $form->handleRequest($request);
-            if($form->isSubmitted() && $form->isValid()){
-                $skill = $form->getData();
-                $skill->setUpdatedAt(new \DateTime());
-                $entityManagerInterface->flush();
-
-                return $this->redirectToRoute('app_admin_skills');
-            }
-            return $this->render('AdminDashboard/Skills/create_skill.html.twig',[
-                'form' => $form->createView()
-            ]);
-        } else {
-            $skill->setIsActive($skill->isIsActive()^true);
+        $form = $this->createForm(SkillType::class,$skill);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $skill = $form->getData();
             $entityManagerInterface->flush();
+
             return $this->redirectToRoute('app_admin_skills');
         }
+        return $this->render('AdminDashboard/Skills/create_skill.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
-//make route for inactie active
+
+    #[Route('/admin/dashboard/skills/updateStatus/{id}', name: 'app_admin_skills_update_status')]
+    public function UpdateSkillStatus(EntityManagerInterface $entityManagerInterface, Skills $skills): Response
+    {
+        $skills->setIsActive($skills->isIsActive()^true);
+        $entityManagerInterface->flush();
+        return $this->redirectToRoute('app_admin_skills');
+    }
+    
     #[Route('/admin/dashboard/skills/delete/{id}', name: 'app_admin_skills_delete')]
-    public function Delete_Skill(EntityManagerInterface $entityManagerInterface, Skills $skill): Response
+    public function DeleteSkill(EntityManagerInterface $entityManagerInterface, Skills $skill): Response
     {
         $skill->setIsDeleted(true);
         $entityManagerInterface->flush();
         return $this->redirectToRoute('app_admin_skills');
     }
 
-    /******************************************DEPARTMENT***************************************************/
 
     #[Route('/admin/dashboard/department/create', name: 'app_admin_department_create')]
     public function CreateDepartment(EntityManagerInterface $entityManagerInterface, Request $request): Response
@@ -104,37 +105,37 @@ class AdminDashboardController extends AbstractDashboardController
     }
 
     #[Route('/admin/dashboard/department', name: 'app_admin_department')]
-    public function Departments(EntityManagerInterface $entityManagerInterface): Response
+    public function Departments(DepartmentRepository $departmentRepository): Response
     {
         return $this->render('AdminDashboard/Department/departments.html.twig',[
-            'departments' => $entityManagerInterface->getRepository(Department::class)->findBy([
+            'departments' => $departmentRepository->findBy([
                 'isDeleted' => false
             ])
         ]);
     }
 
-    #[Route('/admin/dashboard/department/update/{id}/{flag}', name: 'app_admin_department_update')]
-    public function DepartmentUpdate(EntityManagerInterface $entityManagerInterface, Department $department, Request $request, int $flag): Response
+    #[Route('/admin/dashboard/department/update/{id}', name: 'app_admin_department_update')]
+    public function DepartmentUpdate(EntityManagerInterface $entityManagerInterface, Department $department, Request $request): Response
     {
-        if($flag==1) {
-            $form = $this->createForm(DepartmentType::class,$department);
-            $form->handleRequest($request);
-            if($form->isSubmitted() && $form->isValid()){
-                $department = $form->getData();
-                $department->setUpdatedAt(new \DateTime());
-                $entityManagerInterface->flush();
-
-                return $this->redirectToRoute('app_admin_department');
-            }
-            return $this->render('AdminDashboard/Department/create_department.html.twig',[
-                'form' => $form->createView()
-            ]);
-        } else {
-            $department->setIsActive($department->isIsActive()^true);
+        $form = $this->createForm(DepartmentType::class,$department);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $department = $form->getData();
             $entityManagerInterface->flush();
+
             return $this->redirectToRoute('app_admin_department');
         }
-        
+        return $this->render('AdminDashboard/Department/create_department.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/admin/dashboard/department/updateStatus/{id}', name: 'app_admin_department_update_status')]
+    public function DepartmentUpdateStatus(EntityManagerInterface $entityManagerInterface, Department $department): Response
+    {
+        $department->setIsActive($department->isIsActive()^true);
+        $entityManagerInterface->flush();
+        return $this->redirectToRoute('app_admin_department');
     }
 
     #[Route('/admin/dashboard/department/delete/{id}', name: 'app_admin_department_delete')]
